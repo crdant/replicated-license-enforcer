b1ea63ff7c11c3c654901d3c93f8166dcc88e59a
Chuck D'Antonio <chuck@replicated.com>
2024-05-30 16:10:14 -0400

    Provides a client package
    TL;DR
    -----
    
    Implements a client for the needed subset of the Replicated SDK
    
    Details
    -------
    
    Adds a simple Replicated SDK client that handles getting an
    expiration date and understanding if it's been tampered with. That's all
    we need for our purposes so that's all it implements.

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/crdant/replicated-sdk-client
+
+go 1.22.0
+
+toolchain go1.22.3
+
+require github.com/replicatedhq/replicated-sdk v1.0.0-beta.20
---
+++ pkg/client/client.go
@@ -0,0 +1,28 @@
+package client
+
+import (
+    "io"
+    "net/http"
+    "time"
+)
+
+type Client struct {
+    HTTPClient *http.Client
+    BaseURL    string
+}
+
+func NewClient(baseURL string) *Client {
+    return &Client{
+        HTTPClient: &http.Client{Timeout: time.Second * 30},
+        BaseURL:    baseURL,
+    }
+}
+
+// Common method to make requests
+func (c *Client) makeRequest(method, url string, body io.Reader) (*http.Response, error) {
+    req, err := http.NewRequest(method, c.BaseURL+url, body)
+    if err != nil {
+        return nil, err
+    }
+    return c.HTTPClient.Do(req)
+}
