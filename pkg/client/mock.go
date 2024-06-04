25dbfe9eff6f4ddb589aafa30e2a0a5aa09fcf92
Chuck D'Antonio <chuck@replicated.com>
2024-06-04 13:21:47 -0400

    Provides tests for enforce package

--- pkg/client/mock.go
+++ pkg/client/mock.go
@@ -7,7 +7,31 @@ import (
 )
 
 type MockAPIClient struct {
-    mock.Mock
+    name string;
+    slug string;
+    expiration time.Time;
+
+    mock.Mock;
+}
+
+
+func DefaultMockAPIClient() *MockAPIClient {
+    return NewMockAPIClient("Slackernews", "slackernews-mackerel", time.Now().Add(24 * time.Hour))
+}
+
+func NewMockAPIClient(name string, slug string, expiration time.Time) *MockAPIClient {
+    mock := &MockAPIClient{
+        name: name,
+          
+        slug: slug,
+        expiration: expiration,
+    }
+    
+    mock.On("GetAppName").Return(mock.name, nil)
+    mock.On("GetAppSlug").Return(mock.slug, nil)
+    mock.On("GetExpirationDate").Return(mock.expiration, nil)
+
+    return mock
 }
 
 func (m *MockAPIClient) GetExpirationDate() (time.Time, error) {
--- pkg/enforce/enforce_test.go
+++ pkg/enforce/enforce_test.go
@@ -5,14 +5,16 @@ import (
     "testing"
 
     "github.com/crdant/replicated-license-enforcer/pkg/client"
+    "github.com/crdant/replicated-license-enforcer/pkg/events"
 
-    "github.com/stretchr/testify/mock"
+    "github.com/stretchr/testify/assert"
+    "github.com/stretchr/testify/require"
 )
 
+
+
 func TestIsActiveLicenseValid(t *testing.T) {
-    future := time.Now().Add(24 * time.Hour)
-    mockClient := client.newMockAPIClient()
-    mockClient.On("GetExpirationDate", mock.Anything).Return(future, nil)
+    mockClient := client.DefaultMockAPIClient()
 
     valid, err := isValid(mockClient)
     if err != nil {
@@ -25,8 +27,10 @@ func TestIsActiveLicenseValid(t *testing.T) {
 
 func TestIsExpiredLicenseNotValid(t *testing.T) {
     past := time.Now().Add(-24 * time.Hour)
-    mockClient := client.newMockAPIClient()
-    mockClient.On("GetExpirationDate", mock.Anything).Return(past, nil)
+    name := "Slackernews"
+    slug := "slackernews-mackerel"
+
+    mockClient := client.NewMockAPIClient(name, slug, past)
 
     valid, err := isValid(mockClient)
     if err != nil {
@@ -37,14 +41,22 @@ func TestIsExpiredLicenseNotValid(t *testing.T) {
     }
 }
 
-func TestCheckValidLicense(t *testing.T) {
+func TestCheckExpiredLicense(t *testing.T) {
     past := time.Now().Add(-24 * time.Hour)
     name := "Slackernews"
     slug := "slackernews-mackerel"
 
-    mockClient := client.newMockAPIClient()
-    mockClient.On("GetExpirationDate", mock.Anything).Return(past, nil)
-    mockClient.On("GetAppName", mock.Anything).Return(name, nil)
-    mockClient.On("GetAppSlug", mock.Anything).Return(slug, nil)
-  
+    sdkClient := client.NewMockAPIClient(name, slug, past)
+    k8sClient := events.NewMockEventClient()
+    err := check(sdkClient, k8sClient)
+
+    assert.Error(t, err)
+    assert.Len(t, k8sClient.Events, 1)
+
+    event, err := k8sClient.GetLicenseEvent(slug, past)
+    require.NoError(t, err)
+    require.NotNil(t, event)
+
+    assert.Equal(t, "Warning", event.Type)
+    assert.Equal(t, "Expired", event.Reason)
 }
